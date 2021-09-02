bcec9838bd07e76506dcca2b3684ed48ea5a8b18
张德涛 <zdtjss@163.com>
2021-09-02 11:11:57 +0800

    ASM辅助类

--- src/test/java/com/nway/spring/jdbc/MonitorAccess.java
+++ src/test/java/com/nway/spring/jdbc/MonitorAccess.java
@@ -1,36 +1,26 @@
 package com.nway.spring.jdbc;
 
-import com.nway.spring.jdbc.bean.processor.asm.BeanAccess;
+import com.nway.spring.jdbc.bean.processor.BeanAccess;
 import com.nway.spring.jdbc.performance.entity.Monitor;
 
-import java.util.Date;
-
-public class MonitorAccess implements BeanAccess<Monitor> {
+public class MonitorAccess implements BeanAccess {
 
     private Monitor bean;
 
-    public MonitorAccess() {
-    }
-
-    public Monitor newInstance() {
+    public <T> T newInstance() {
         this.bean = new Monitor();
-        return this.bean;
+        return (T) this.bean;
     }
 
     public void setVal(String fieldName, Object val) {
         if ("id".equals(fieldName)) {
             this.bean.setId((Integer) val);
-        } else if ("brand".equals(fieldName)) {
-            this.bean.setBrand((String) val);
-        } else if ("model".equals(fieldName)) {
-            this.bean.setModel((String) val);
-        } else if ("price".equals(fieldName)) {
-            this.bean.setPrice((Float) val);
         } else if ("type".equals(fieldName)) {
-            this.bean.setType((Integer) val);
-        } else if ("maxResolution".equals(fieldName)) {
-            this.bean.setMaxResolution((String) val);
+            this.bean.setType((int) val);
+        } else if ("photo".equals(fieldName)) {
+            this.bean.setPhoto((byte[]) val);
         }
+
     }
 
 }
--- src/test/java/com/nway/spring/jdbc/MonitorAccessDump.java
+++ src/test/java/com/nway/spring/jdbc/MonitorAccessDump.java
@@ -1,7 +1,8 @@
 package com.nway.spring.jdbc;
 
-import com.nway.spring.jdbc.bean.processor.asm.BeanAccess;
+import com.nway.spring.jdbc.bean.processor.BeanAccess;
 import com.nway.spring.jdbc.bean.processor.asm.DynamicBeanClassLoader;
+import com.nway.spring.jdbc.performance.entity.Monitor;
 import org.objectweb.asm.*;
 import org.springframework.util.ClassUtils;
 
@@ -12,6 +13,7 @@ public class MonitorAccessDump implements Opcodes {
             DynamicBeanClassLoader beanClassLoader = new DynamicBeanClassLoader(ClassUtils.getDefaultClassLoader(), "D:\\com\\nway\\spring\\jdbc\\MonitorAccess");
             Class<?> processor = beanClassLoader.defineClass("com.nway.spring.jdbc.MonitorAccess", dump());
             BeanAccess beanAccess = (BeanAccess) processor.getConstructor().newInstance();
+            Monitor o = (Monitor) beanAccess.newInstance();
         } catch (Exception e) {
             throw new RuntimeException("使用ASM创建 [ className ] 失败", e);
         }
@@ -25,7 +27,7 @@ public class MonitorAccessDump implements Opcodes {
         MethodVisitor methodVisitor;
         AnnotationVisitor annotationVisitor0;
 
-        classWriter.visit(V1_8, ACC_PUBLIC | ACC_SUPER, "com/nway/spring/jdbc/MonitorAccess", "Ljava/lang/Object;Lcom/nway/spring/jdbc/bean/processor/asm/BeanAccess<Lcom/nway/spring/jdbc/performance/entity/Monitor;>;", "java/lang/Object", new String[]{"com/nway/spring/jdbc/bean/processor/asm/BeanAccess"});
+        classWriter.visit(V1_8, ACC_PUBLIC | ACC_SUPER, "com/nway/spring/jdbc/MonitorAccess", null, "java/lang/Object", new String[]{"com/nway/spring/jdbc/bean/processor/asm/BeanAccess"});
 
         classWriter.visitSource("MonitorAccess.java", null);
 
@@ -38,25 +40,22 @@ public class MonitorAccessDump implements Opcodes {
             methodVisitor.visitCode();
             Label label0 = new Label();
             methodVisitor.visitLabel(label0);
-            methodVisitor.visitLineNumber(12, label0);
+            methodVisitor.visitLineNumber(8, label0);
             methodVisitor.visitVarInsn(ALOAD, 0);
             methodVisitor.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
+            methodVisitor.visitInsn(RETURN);
             Label label1 = new Label();
             methodVisitor.visitLabel(label1);
-            methodVisitor.visitLineNumber(13, label1);
-            methodVisitor.visitInsn(RETURN);
-            Label label2 = new Label();
-            methodVisitor.visitLabel(label2);
-            methodVisitor.visitLocalVariable("this", "Lcom/nway/spring/jdbc/MonitorAccess;", null, label0, label2, 0);
+            methodVisitor.visitLocalVariable("this", "Lcom/nway/spring/jdbc/MonitorAccess;", null, label0, label1, 0);
             methodVisitor.visitMaxs(1, 1);
             methodVisitor.visitEnd();
         }
         {
-            methodVisitor = classWriter.visitMethod(ACC_PUBLIC, "newInstance", "()Lcom/nway/spring/jdbc/performance/entity/Monitor;", null, null);
+            methodVisitor = classWriter.visitMethod(ACC_PUBLIC, "newInstance", "()Ljava/lang/Object;", "<T:Ljava/lang/Object;>()TT;", null);
             methodVisitor.visitCode();
             Label label0 = new Label();
             methodVisitor.visitLabel(label0);
-            methodVisitor.visitLineNumber(16, label0);
+            methodVisitor.visitLineNumber(13, label0);
             methodVisitor.visitVarInsn(ALOAD, 0);
             methodVisitor.visitTypeInsn(NEW, "com/nway/spring/jdbc/performance/entity/Monitor");
             methodVisitor.visitInsn(DUP);
@@ -64,7 +63,7 @@ public class MonitorAccessDump implements Opcodes {
             methodVisitor.visitFieldInsn(PUTFIELD, "com/nway/spring/jdbc/MonitorAccess", "bean", "Lcom/nway/spring/jdbc/performance/entity/Monitor;");
             Label label1 = new Label();
             methodVisitor.visitLabel(label1);
-            methodVisitor.visitLineNumber(17, label1);
+            methodVisitor.visitLineNumber(14, label1);
             methodVisitor.visitVarInsn(ALOAD, 0);
             methodVisitor.visitFieldInsn(GETFIELD, "com/nway/spring/jdbc/MonitorAccess", "bean", "Lcom/nway/spring/jdbc/performance/entity/Monitor;");
             methodVisitor.visitInsn(ARETURN);
@@ -80,27 +79,25 @@ public class MonitorAccessDump implements Opcodes {
 
             Label label0 = new Label();
             methodVisitor.visitLabel(label0);
-            methodVisitor.visitLineNumber(21, label0);
+            methodVisitor.visitLineNumber(18, label0);
             methodVisitor.visitLdcInsn("id");
             methodVisitor.visitVarInsn(ALOAD, 1);
             methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/lang/String", "equals", "(Ljava/lang/Object;)Z", false);
             Label label1 = new Label();
             methodVisitor.visitJumpInsn(IFEQ, label1);
-
             Label label2 = new Label();
             methodVisitor.visitLabel(label2);
-            methodVisitor.visitLineNumber(22, label2);
+            methodVisitor.visitLineNumber(19, label2);
             methodVisitor.visitVarInsn(ALOAD, 0);
             methodVisitor.visitFieldInsn(GETFIELD, "com/nway/spring/jdbc/MonitorAccess", "bean", "Lcom/nway/spring/jdbc/performance/entity/Monitor;");
             methodVisitor.visitVarInsn(ALOAD, 2);
             methodVisitor.visitTypeInsn(CHECKCAST, "java/lang/Integer");
-            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/lang/Integer", "intValue", "()I", false);
-            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "com/nway/spring/jdbc/performance/entity/Monitor", "setId", "(I)V", false);
+            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "com/nway/spring/jdbc/performance/entity/Monitor", "setId", "(Ljava/lang/Integer;)V", false);
+
             Label label3 = new Label();
             methodVisitor.visitJumpInsn(GOTO, label3);
             methodVisitor.visitLabel(label1);
-            methodVisitor.visitLineNumber(23, label1);
-
+            methodVisitor.visitLineNumber(20, label1);
             methodVisitor.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
 
             methodVisitor.visitLdcInsn("brand");
@@ -110,7 +107,7 @@ public class MonitorAccessDump implements Opcodes {
             methodVisitor.visitJumpInsn(IFEQ, label4);
             Label label5 = new Label();
             methodVisitor.visitLabel(label5);
-            methodVisitor.visitLineNumber(24, label5);
+            methodVisitor.visitLineNumber(21, label5);
             methodVisitor.visitVarInsn(ALOAD, 0);
             methodVisitor.visitFieldInsn(GETFIELD, "com/nway/spring/jdbc/MonitorAccess", "bean", "Lcom/nway/spring/jdbc/performance/entity/Monitor;");
             methodVisitor.visitVarInsn(ALOAD, 2);
@@ -118,103 +115,52 @@ public class MonitorAccessDump implements Opcodes {
             methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "com/nway/spring/jdbc/performance/entity/Monitor", "setBrand", "(Ljava/lang/String;)V", false);
             methodVisitor.visitJumpInsn(GOTO, label3);
             methodVisitor.visitLabel(label4);
-            methodVisitor.visitLineNumber(25, label4);
-
+            methodVisitor.visitLineNumber(22, label4);
             methodVisitor.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
-            methodVisitor.visitLdcInsn("model");
+
+            methodVisitor.visitLdcInsn("photo");
             methodVisitor.visitVarInsn(ALOAD, 1);
             methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/lang/String", "equals", "(Ljava/lang/Object;)Z", false);
             Label label6 = new Label();
             methodVisitor.visitJumpInsn(IFEQ, label6);
             Label label7 = new Label();
             methodVisitor.visitLabel(label7);
-            methodVisitor.visitLineNumber(26, label7);
+            methodVisitor.visitLineNumber(23, label7);
             methodVisitor.visitVarInsn(ALOAD, 0);
             methodVisitor.visitFieldInsn(GETFIELD, "com/nway/spring/jdbc/MonitorAccess", "bean", "Lcom/nway/spring/jdbc/performance/entity/Monitor;");
             methodVisitor.visitVarInsn(ALOAD, 2);
-            methodVisitor.visitTypeInsn(CHECKCAST, "java/lang/String");
-            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "com/nway/spring/jdbc/performance/entity/Monitor", "setModel", "(Ljava/lang/String;)V", false);
+            methodVisitor.visitTypeInsn(CHECKCAST, "[B");
+            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "com/nway/spring/jdbc/performance/entity/Monitor", "setPhoto", "([B)V", false);
             methodVisitor.visitJumpInsn(GOTO, label3);
             methodVisitor.visitLabel(label6);
-            methodVisitor.visitLineNumber(27, label6);
-
+            methodVisitor.visitLineNumber(24, label6);
             methodVisitor.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
-            methodVisitor.visitLdcInsn("price");
-            methodVisitor.visitVarInsn(ALOAD, 1);
-            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/lang/String", "equals", "(Ljava/lang/Object;)Z", false);
-            Label label8 = new Label();
-            methodVisitor.visitJumpInsn(IFEQ, label8);
-            Label label9 = new Label();
-            methodVisitor.visitLabel(label9);
-            methodVisitor.visitLineNumber(28, label9);
-            methodVisitor.visitVarInsn(ALOAD, 0);
-            methodVisitor.visitFieldInsn(GETFIELD, "com/nway/spring/jdbc/MonitorAccess", "bean", "Lcom/nway/spring/jdbc/performance/entity/Monitor;");
-            methodVisitor.visitVarInsn(ALOAD, 2);
-            methodVisitor.visitTypeInsn(CHECKCAST, "java/lang/Float");
-            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/lang/Float", "floatValue", "()F", false);
-            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "com/nway/spring/jdbc/performance/entity/Monitor", "setPrice", "(F)V", false);
-            methodVisitor.visitJumpInsn(GOTO, label3);
-            methodVisitor.visitLabel(label8);
-            methodVisitor.visitLineNumber(29, label8);
 
-            methodVisitor.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
-            methodVisitor.visitLdcInsn("type");
-            methodVisitor.visitVarInsn(ALOAD, 1);
-            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/lang/String", "equals", "(Ljava/lang/Object;)Z", false);
-            Label label10 = new Label();
-            methodVisitor.visitJumpInsn(IFEQ, label10);
-            Label label11 = new Label();
-            methodVisitor.visitLabel(label11);
-            methodVisitor.visitLineNumber(30, label11);
-            methodVisitor.visitVarInsn(ALOAD, 0);
-            methodVisitor.visitFieldInsn(GETFIELD, "com/nway/spring/jdbc/MonitorAccess", "bean", "Lcom/nway/spring/jdbc/performance/entity/Monitor;");
-            methodVisitor.visitVarInsn(ALOAD, 2);
-            methodVisitor.visitTypeInsn(CHECKCAST, "java/lang/Integer");
-            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/lang/Integer", "intValue", "()I", false);
-            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "com/nway/spring/jdbc/performance/entity/Monitor", "setType", "(I)V", false);
-            methodVisitor.visitJumpInsn(GOTO, label3);
-            methodVisitor.visitLabel(label10);
-            methodVisitor.visitLineNumber(31, label10);
-
-            methodVisitor.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
-            methodVisitor.visitLdcInsn("maxResolution");
+           /* methodVisitor.visitLdcInsn("photo");
             methodVisitor.visitVarInsn(ALOAD, 1);
             methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/lang/String", "equals", "(Ljava/lang/Object;)Z", false);
             methodVisitor.visitJumpInsn(IFEQ, label3);
-            Label label12 = new Label();
-            methodVisitor.visitLabel(label12);
-            methodVisitor.visitLineNumber(32, label12);
+            Label label8 = new Label();
+            methodVisitor.visitLabel(label8);
+            methodVisitor.visitLineNumber(25, label8);
             methodVisitor.visitVarInsn(ALOAD, 0);
             methodVisitor.visitFieldInsn(GETFIELD, "com/nway/spring/jdbc/MonitorAccess", "bean", "Lcom/nway/spring/jdbc/performance/entity/Monitor;");
             methodVisitor.visitVarInsn(ALOAD, 2);
-            methodVisitor.visitTypeInsn(CHECKCAST, "java/lang/String");
-            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "com/nway/spring/jdbc/performance/entity/Monitor", "setMaxResolution", "(Ljava/lang/String;)V", false);
+            methodVisitor.visitTypeInsn(CHECKCAST, "[B");
+            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "com/nway/spring/jdbc/performance/entity/Monitor", "setPhoto", "([B)V", false);
+*/
             methodVisitor.visitLabel(label3);
-            methodVisitor.visitLineNumber(34, label3);
-
+            methodVisitor.visitLineNumber(27, label3);
             methodVisitor.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
+
             methodVisitor.visitInsn(RETURN);
-            Label label13 = new Label();
-            methodVisitor.visitLabel(label13);
-            methodVisitor.visitLocalVariable("this", "Lcom/nway/spring/jdbc/MonitorAccess;", null, label0, label13, 0);
-            methodVisitor.visitLocalVariable("fieldName", "Ljava/lang/String;", null, label0, label13, 1);
-            methodVisitor.visitLocalVariable("val", "Ljava/lang/Object;", null, label0, label13, 2);
+
+            Label label9 = new Label();
+            methodVisitor.visitLabel(label9);
+            methodVisitor.visitLocalVariable("this", "Lcom/nway/spring/jdbc/MonitorAccess;", null, label0, label9, 0);
+            methodVisitor.visitLocalVariable("fieldName", "Ljava/lang/String;", null, label0, label9, 1);
+            methodVisitor.visitLocalVariable("val", "Ljava/lang/Object;", null, label0, label9, 2);
             methodVisitor.visitMaxs(2, 3);
-            methodVisitor.visitEnd();
-        }
-        {
-            methodVisitor = classWriter.visitMethod(ACC_PUBLIC | ACC_BRIDGE | ACC_SYNTHETIC, "newInstance", "()Ljava/lang/Object;", null, null);
-            methodVisitor.visitCode();
-            Label label0 = new Label();
-            methodVisitor.visitLabel(label0);
-            methodVisitor.visitLineNumber(8, label0);
-            methodVisitor.visitVarInsn(ALOAD, 0);
-            methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "com/nway/spring/jdbc/MonitorAccess", "newInstance", "()Lcom/nway/spring/jdbc/performance/entity/Monitor;", false);
-            methodVisitor.visitInsn(ARETURN);
-            Label label1 = new Label();
-            methodVisitor.visitLabel(label1);
-            methodVisitor.visitLocalVariable("this", "Lcom/nway/spring/jdbc/MonitorAccess;", null, label0, label1, 0);
-            methodVisitor.visitMaxs(1, 1);
             methodVisitor.visitEnd();
         }
         classWriter.visitEnd();
