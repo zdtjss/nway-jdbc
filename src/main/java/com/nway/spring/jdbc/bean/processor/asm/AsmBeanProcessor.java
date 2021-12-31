c5f6c1c240856e5e7fe28b27b07a100a81835ed4
张德涛 <zdtjss@163.com>
2022-01-01 00:27:20 +0800

    ASM 支持自定义cacheKey

--- src/main/java/com/nway/spring/jdbc/bean/processor/asm/AsmBeanProcessor.java
+++ src/main/java/com/nway/spring/jdbc/bean/processor/asm/AsmBeanProcessor.java
@@ -2,32 +2,31 @@ package com.nway.spring.jdbc.bean.processor.asm;
 
 import com.nway.spring.jdbc.bean.processor.BeanProcessor;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.JdbcUtils;
 
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.function.Function;
 
 public class AsmBeanProcessor implements BeanProcessor {
 
     private static final ConcurrentMap<String, AsmRowMapper> localCache = new ConcurrentHashMap<>(256);
 
+    private Function<ResultSet, String> sqlExtractor;
+
     @Override
     public <T> List<T> toBeanList(ResultSet rs, Class<T> mappedClass) throws SQLException {
 
         int rowNum = 0;
-        LinkedHashMap<String, Integer> columnIndexMap = getColumnIndex(rs);
-        String cacheKey = columnIndexMap.keySet().toString() + mappedClass.hashCode();
 
-        AsmRowMapper<T> mapper = (AsmRowMapper<T>) Optional.ofNullable(localCache.get(cacheKey))
-                .orElseGet(() -> {
-                    AsmRowMapper<T> rowMapper = new AsmRowMapper<>(mappedClass, columnIndexMap);
-                    localCache.put(cacheKey, rowMapper);
-                    return rowMapper;
-                });
+        RowMapper<T> mapper = getMapper(rs, mappedClass);
 
         final List<T> results = new ArrayList<>();
         while (rs.next()) {
@@ -40,22 +39,40 @@ public class AsmBeanProcessor implements BeanProcessor {
     @Override
     public <T> T toBean(ResultSet rs, Class<T> mappedClass) throws SQLException {
 
-        LinkedHashMap<String, Integer> columnIndex = getColumnIndex(rs);
-
-        String cacheKey = columnIndex.keySet().toString() + mappedClass.hashCode();
-
-        AsmRowMapper<T> mapper = (AsmRowMapper<T>) Optional.ofNullable(localCache.get(cacheKey))
-                .orElseGet(() -> {
-                    AsmRowMapper<T> rowMapper = new AsmRowMapper<T>(mappedClass, columnIndex);
-                    localCache.put(cacheKey, rowMapper);
-                    return rowMapper;
-                });
-
+        RowMapper<T> mapper = getMapper(rs, mappedClass);
         T row = mapper.mapRow(rs, 0);
         if (rs.next()) {
             throw new IncorrectResultSizeDataAccessException("发现了多条符合条件的数据", 1);
         }
         return row;
+    }
+
+    public void setSqlExtractor(Function<ResultSet, String> extractor) {
+        this.sqlExtractor = extractor;
+    }
+
+    private <T> RowMapper<T> getMapper(ResultSet rs, Class<T> mappedClass) throws SQLException {
+
+        String cacheKey = null;
+        LinkedHashMap<String, Integer> columnIndexMap = null;
+        if(sqlExtractor != null) {
+            cacheKey = sqlExtractor.apply(rs);
+        }
+
+        AsmRowMapper<T> mapper = null;
+        if (cacheKey != null) {
+            mapper = localCache.get(cacheKey);
+        } else {
+            columnIndexMap = getColumnIndex(rs);
+            cacheKey = columnIndexMap.keySet().toString() + mappedClass.hashCode();
+        }
+
+        if(mapper == null) {
+            columnIndexMap = columnIndexMap == null ? getColumnIndex(rs) : columnIndexMap;
+            mapper = new AsmRowMapper<>(mappedClass, columnIndexMap);
+            localCache.put(cacheKey, mapper);
+        }
+        return mapper;
     }
 
     private LinkedHashMap<String, Integer> getColumnIndex(ResultSet rs) throws SQLException {
