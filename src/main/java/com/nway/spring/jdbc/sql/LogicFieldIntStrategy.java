e36916da8f37bca1beb0f0b8e18cc3be8db8ea67
张德涛 <zhangdetao@shinho.net.cn>
2022-07-11 10:00:24 +0800

    默认的逻辑删除标识符类型有int改为了更合适的boolean  为了兼容历史版本也考虑实际存在的情况提供int型逻辑删除

---
+++ src/main/java/com/nway/spring/jdbc/sql/LogicFieldIntStrategy.java
@@ -0,0 +1,29 @@
+package com.nway.spring.jdbc.sql;
+
+import com.nway.spring.jdbc.sql.fill.FillStrategy;
+import com.nway.spring.jdbc.sql.permission.PermissionStrategy;
+import com.nway.spring.jdbc.sql.permission.WhereCondition;
+
+/**
+ * 逻辑删除
+ */
+public class LogicFieldIntStrategy implements FillStrategy, PermissionStrategy {
+
+    @Override
+    public boolean isSupport(SqlType sqlType) {
+        return true;
+    }
+
+    @Override
+    public Object getValue(SqlType sqlType, Object val) {
+        if (val == DEFAULT_NONE || val == null) {
+            return SqlType.DELETE == sqlType ? 1 : 0;
+        }
+        return val;
+    }
+
+    @Override
+    public WhereCondition getSqlSegment(String column) {
+        return new WhereCondition(column + " = ? ", 0);
+    }
+}
